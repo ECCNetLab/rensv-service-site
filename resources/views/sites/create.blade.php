552aab63059a9c0b65c6d006b9d9b7aea0690af0
junjun <junjun@example.com>
2020-11-05 10:10:45 +0900

    [edit] サイト作成時に必要な項目にrequiredつけた

--- resources/views/sites/create.blade.php
+++ resources/views/sites/create.blade.php
@@ -12,13 +12,13 @@
                         {{ csrf_field() }}
                         <div class="form-group">
                             <label for="siteName">サイト名</label>
-                            <input type="text" class="form-control" name="name" id="siteName" aria-describedby="siteName" placeholder="サイト名">
+                            <input type="text" class="form-control" name="name" id="siteName" aria-describedby="siteName" placeholder="サイト名" required>
                             <small id="siteName" class="form-text text-muted">アルファベット大文字小文字。ftpのユーザー名にもなります。</small>
                         </div>
                         <div class="form-group">
                             <label for="domainName">ドメイン名</label>
                             <div class="input-group">
-                                <input type="text" class="form-control" name="domain" id="domainName" aria-describedby="domainName" placeholder="ドメイン名">
+                                <input type="text" class="form-control" name="domain" id="domainName" aria-describedby="domainName" placeholder="ドメイン名" required>
                                 <div class="input-group-append">
                                     <span class="input-group-text" id="basic-addon2">.{{ config('rensv.domain') }}</span>
                                 </div>
@@ -27,18 +27,18 @@
                         </div>
                         <div class="form-group">
                             <label for="ftpPassword">FTP パスワード</label>
-                            <input type="password" class="form-control" name="ftpPassword" id="ftpPassword" aria-describedby="ftpPassword" placeholder="FTPパスワード">
+                            <input type="password" class="form-control" name="ftpPassword" id="ftpPassword" aria-describedby="ftpPassword" placeholder="FTPパスワード" required>
                         </div>
                         <div class="form-group">
                             <label for="reFtpPassword">FTP パスワード(確認)</label>
-                            <input type="password" class="form-control" name="reFtpPassword" id="reFtpPassword" aria-describedby="reFtpPassword" placeholder="FTPパスワード(確認)">
+                            <input type="password" class="form-control" name="reFtpPassword" id="reFtpPassword" aria-describedby="reFtpPassword" placeholder="FTPパスワード(確認)" required>
                         </div>
                         <div class="form-group">
                         <label>プラン</label>
                             <div class="btn-group btn-group-toggle" data-toggle="buttons">
                                 @foreach($plans as $p)
                                 <label class="btn btn-secondary">
-                                    <input type="radio" name="plan" id="{{$p->id}}" value="{{$p->id}}" autocomplete="off"> {{$p->name}}: {{$p->price}}円
+                                    <input type="radio" name="plan" id="{{$p->id}}" value="{{$p->id}}" autocomplete="off" required> {{$p->name}}: {{$p->price}}円
                                 </label>
                                 @endforeach
                             </div>
