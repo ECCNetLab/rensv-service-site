894cb32e1360964b310b837823f67cc792ed1cd8
junjun <junjun@example.com>
2020-11-11 14:47:13 +0900

    [add] .envに設定を持たせられるようにした

--- config/amqp.php
+++ config/amqp.php
@@ -6,13 +6,13 @@ return [
     'properties' => [
 
         'production' => [
-            'host'                => 'localhost',
-            'port'                => 5672,
-            'username'            => 'guest',
-            'password'            => 'guest',
-            'vhost'               => '/',
-            'exchange'            => 'test',
-            'exchange_type'       => 'direct',
+            'host'           => env('AMQP_HOST', 'localhost'),
+            'port'           => env('AMQP_PORT', 5672),
+            'username'       => env('AMQP_USERNAME', 'guest'),
+            'password'       => env('AMQP_PASSWORD', 'guest'),
+            'vhost'          => env('AMQP_VHOST', '/'),
+            'exchange'       => env('AMQP_EXCHANGE', 'test'),
+            'exchange_type'  => env('AMQP_EXCHANGE_TYPE', 'direct'),
         ],
 
     ],
